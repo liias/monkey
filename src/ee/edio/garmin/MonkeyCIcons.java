7121d963f94c716b79e6267d1b4334fe6a2dd3e4
kersurk <madisliias@gmail.com>
2015-08-14 00:11:56 +0300

    add some icons

--- src/ee/edio/garmin/MonkeyCIcons.java
+++ src/ee/edio/garmin/MonkeyCIcons.java
@@ -1,6 +1,5 @@
 package ee.edio.garmin;
 
-import com.intellij.icons.AllIcons;
 import com.intellij.openapi.util.IconLoader;
 
 import javax.swing.*;
@@ -9,5 +8,7 @@ public class MonkeyCIcons {
   public static final Icon FILE = IconLoader.getIcon("/ee/edio/garmin/icons/mc_ext.png");
   public static final Icon SDK = IconLoader.getIcon("/ee/edio/garmin/icons/sdk.png");
   public static final Icon ADD_SDK = IconLoader.getIcon("/ee/edio/garmin/icons/addsdk.png");
+  public static final Icon MODULE24 = IconLoader.getIcon("/ee/edio/garmin/icons/module.png"); // 24
+  public static final Icon MODULE16 = IconLoader.getIcon("/ee/edio/garmin/icons/sdk.png"); // 24
   //module type = 24x24
 }
--- src/ee/edio/garmin/MonkeyCModuleType.java
+++ src/ee/edio/garmin/MonkeyCModuleType.java
@@ -1,6 +1,5 @@
 package ee.edio.garmin;
 
-import com.intellij.icons.AllIcons;
 import com.intellij.openapi.module.ModuleType;
 import com.intellij.openapi.module.ModuleTypeManager;
 import org.jetbrains.annotations.NotNull;
@@ -38,11 +37,11 @@ public class MonkeyCModuleType extends ModuleType<MonkeyCModuleBuilder> {
 
   @Override
   public Icon getBigIcon() {
-    return AllIcons.Modules.Types.EmptyProjectType;
-  }
+    return MonkeyCIcons.MODULE24;
+  } //24
 
   @Override
   public Icon getNodeIcon(@Deprecated boolean isOpened) {
-    return AllIcons.Nodes.Module;
-  }
+    return MonkeyCIcons.MODULE16;
+  } // 16
 }
