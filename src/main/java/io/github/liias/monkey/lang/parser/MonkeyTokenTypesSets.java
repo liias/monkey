6e5018a4e2b1e2f68bb2ec287717c77fee77c346
Madis Liias <madisliias@gmail.com>
2016-08-07 15:28:05 +0300

    Fix invalid syntax error on block comment

--- src/main/java/io/github/liias/monkey/lang/parser/MonkeyTokenTypesSets.java
+++ src/main/java/io/github/liias/monkey/lang/parser/MonkeyTokenTypesSets.java
@@ -6,36 +6,36 @@ import io.github.liias.monkey.lang.psi.MonkeyTypes;
 
 public interface MonkeyTokenTypesSets {
   TokenSet STRINGS = TokenSet.create(MonkeyTypes.STRING);
-  TokenSet COMMENTS = TokenSet.create(MonkeyTypes.SINGLE_LINE_COMMENT, MonkeyTypes.SINGLE_LINE_DOC_COMMENT);
+  TokenSet COMMENTS = TokenSet.create(MonkeyTypes.SINGLE_LINE_COMMENT, MonkeyTypes.SINGLE_LINE_DOC_COMMENT, MonkeyTypes.BLOCK_COMMENT);
   TokenSet WHITE_SPACES = TokenSet.create(TokenType.WHITE_SPACE);
 
   TokenSet BUILT_IN_IDENTIFIERS = TokenSet.create(
-      MonkeyTypes.AND,
-      MonkeyTypes.AS,
-      MonkeyTypes.CLASS,
-      MonkeyTypes.CONST,
-      MonkeyTypes.DO,
-      MonkeyTypes.ELSE,
-      MonkeyTypes.ENUM,
-      MonkeyTypes.EXTENDS,
-      MonkeyTypes.FALSE,
-      MonkeyTypes.FOR,
-      MonkeyTypes.FUNCTION,
-      MonkeyTypes.HAS,
-      MonkeyTypes.HIDDEN,
-      MonkeyTypes.IF,
-      MonkeyTypes.INSTANCEOF,
-      MonkeyTypes.MODULE,
-      MonkeyTypes.NATIVE,
-      MonkeyTypes.NEW,
-      MonkeyTypes.NULL,
-      MonkeyTypes.OR,
-      MonkeyTypes.RETURN,
-      MonkeyTypes.STATIC,
-      MonkeyTypes.TRUE,
-      MonkeyTypes.USING,
-      MonkeyTypes.VAR,
-      MonkeyTypes.WHILE
+    MonkeyTypes.AND,
+    MonkeyTypes.AS,
+    MonkeyTypes.CLASS,
+    MonkeyTypes.CONST,
+    MonkeyTypes.DO,
+    MonkeyTypes.ELSE,
+    MonkeyTypes.ENUM,
+    MonkeyTypes.EXTENDS,
+    MonkeyTypes.FALSE,
+    MonkeyTypes.FOR,
+    MonkeyTypes.FUNCTION,
+    MonkeyTypes.HAS,
+    MonkeyTypes.HIDDEN,
+    MonkeyTypes.IF,
+    MonkeyTypes.INSTANCEOF,
+    MonkeyTypes.MODULE,
+    MonkeyTypes.NATIVE,
+    MonkeyTypes.NEW,
+    MonkeyTypes.NULL,
+    MonkeyTypes.OR,
+    MonkeyTypes.RETURN,
+    MonkeyTypes.STATIC,
+    MonkeyTypes.TRUE,
+    MonkeyTypes.USING,
+    MonkeyTypes.VAR,
+    MonkeyTypes.WHILE
   );
 
 
