74110a59212be013655afa81db9efce817ce26c7
Madis Liias <madisliias@gmail.com>
2016-08-30 01:57:15 +0300

    Use "wine cmd /c" instead of "wine" to run .bat files with wine

--- src/main/java/io/github/liias/monkey/Utils.java
+++ src/main/java/io/github/liias/monkey/Utils.java
@@ -1,5 +1,6 @@
 package io.github.liias.monkey;
 
+import com.google.common.collect.ImmutableList;
 import com.intellij.execution.configurations.GeneralCommandLine;
 import com.intellij.openapi.util.SystemInfo;
 import com.intellij.openapi.util.SystemInfoRt;
@@ -8,10 +9,17 @@ import com.intellij.openapi.vfs.CharsetToolkit;
 public class Utils {
   public static GeneralCommandLine createGeneralCommandLine(String workDirectory, String exePath) {
     if (SystemInfo.isLinux) {
+      // wine bla.exe
+      // wine cmd /c bla.bat
+      ImmutableList.Builder<String> paramsBuilder = ImmutableList.builder();
+      if (exePath.endsWith(".bat")) {
+        paramsBuilder.add("cmd", "/c");
+      }
+      paramsBuilder.add(exePath);
       return new GeneralCommandLine()
         .withWorkDirectory(workDirectory)
         .withExePath("wine")
-        .withParameters(exePath)
+        .withParameters(paramsBuilder.build())
         .withCharset(CharsetToolkit.UTF8_CHARSET);
     }
 
